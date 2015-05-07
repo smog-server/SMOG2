bc78e77274c2273d4e8c25cdb093df0d1ae8a6fd
Paul Whitford <p.whitford@neu.edu>
2015-05-07 00:31:52 +0000

    There was a missing requirement in the list of necessary modules.

--- config.bash
+++ config.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-#echo "INSTALLING SMOGV2"
+#echo "Configuring SMOGV2"
 #echo
-#echo "NOTE: MAKE SURE YOU HAVE INSTALLED XML::Simple, Data::Dumper, Exporter and perl PDL"
+#echo "NOTE: MAKE SURE YOU HAVE INSTALLED String::Util XML::Simple, Data::Dumper, Exporter and perl PDL"
 #echo
 #echo "SETTING UP PROPER ENVIRONMENTAL VARIABLES"
 curPath=$1
