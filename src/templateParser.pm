bdba3c5814abc275d9b1b2a1566283b913ebfba8
Paul Whitford <p.whitford@neu.edu>
2016-04-02 18:13:13 -0400

    expanded fatal error message

--- src/templateParser.pm
+++ src/templateParser.pm
@@ -105,7 +105,7 @@ sub smog_quit
 	if($main::noexit){
 		warn("$LINE");
 	}else{
-		print "\n\nFATAL ERROR:  $LINE\n\nNOTE: For diagnostic purposes, you can try to ignore the error by providing the flag -warnonly.\n      This will allow SMOG to proceed as far as possible before exiting.\n      However, it is not recommended that top files generated with this flag be used for an actual simulation.\n";
+		print "\n\nFATAL ERROR:  $LINE\n\nFor more information about specific errors, you can check the FAQ page on smog-server.org,\nthe SMOG2 manual, or you can email us at info\@smog-server.org. \n\nNOTE: For diagnostic purposes, you can try to ignore the error by providing the flag -warnonly.\n      This will allow SMOG to proceed as far as possible before exiting.\n      However, it is not recommended that top files generated with this flag be used for an actual simulation.\n";
 		exit;
 	}
 }
