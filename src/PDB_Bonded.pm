b0fc5297a51ec880850a4878a127b9d4c0f08456
Paul Whitford <p.whitford@neu.edu>
2017-12-15 00:48:44 -0500

    added connect no option for residues

--- src/PDB_Bonded.pm
+++ src/PDB_Bonded.pm
@@ -716,10 +716,10 @@ sub checkconnection
 	if(!exists $connections{$residues{$c0}->{"residueType"}}->{$residues{$c1}->{"residueType"}}){
 		my $typeA=$residues{$c0}->{"residueType"};
 		my $typeB=$residues{$c1}->{"residueType"};
-		my $ii=$i-1;
+		my $ii=$i+1;
 		if((!defined $residues{$c0}->{"connect"} || $residues{$c0}->{"connect"} eq "yes") && (!defined $residues{$c1}->{"connect"} || $residues{$c1}->{"connect"} eq "yes")){
 			print $c0;
-			smog_quit("Connection not defined between residues of type $typeA and $typeB. Check .bif file. Issue encountered when connecting residue $ii and $i in chain $chid (residue index within chain, starting at 0)")
+			smog_quit("Connection not defined between residues of type $typeA and $typeB. Check .bif file. Issue encountered when connecting residue $i and $ii in chain $chid (residue index within chain, starting at 0)")
 		}elsif(defined $residues{$c0}->{"connect"} && $residues{$c0}->{"connect"} ne "no"){
 			my $tmp=$residues{$c0}->{"connect"};
 			smog_quit("residue connect value must be yes, or no.  $tmp found.")
