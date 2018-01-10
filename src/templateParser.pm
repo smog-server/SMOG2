840d0f296fdd82c457935f254c9a2e3d9a0fbafe
Paul Whitford <p.whitford@neu.edu>
2018-01-09 21:02:04 -0500

    smog2: removed tooShort option

--- src/templateParser.pm
+++ src/templateParser.pm
@@ -529,6 +529,17 @@ sub parseSif {
 	$interactionThreshold->{"angles"}={"smallAngles"=>$anglesThreshold->{"smallAngles"},"largeAngles"=>$anglesThreshold->{"largeAngles"}};
 	$interactionThreshold->{"contacts"}={"shortContacts"=>$contactsThreshold->{"shortContacts"}};
 	$interactionThreshold->{"distance"}={"tooShortDistance"=>$distanceThreshold->{"tooShortDistance"}};
+
+
+# depricated options.  We have left them in the schemas so that the error that would be thrown when using an old template will not be cryptic.  However, the entries are optional, and they flag 
+#
+	if( $interactionThreshold->{"distance"}->{"tooShortDistance"} )
+	{
+		smog_quit("tooShortDistance found in .sif file. The use of tooShortDistance has been replaced with bondsThreshold. Please remove tooShortDistance from your .sif");
+	} 
+
+
+
 }
 
 ## [bonds] ai aj fType r0 Kb
