2e5e73e6da195a351422bf9c27ff3814139b3326
Paul Whitford <p.whitford@neu.edu>
2018-10-23 01:07:56 -0400

    smog2: ligand energy scales fixed

--- src/setRatios.pm
+++ src/setRatios.pm
@@ -142,6 +142,10 @@ sub adjustFactorsHelper1
 	$b = sclr(slice($inputPDL,"3:3,$b,:"));
 	$c = sclr(slice($inputPDL,"3:3,$c,:"));
 	$d = sclr(slice($inputPDL,"3:3,$d,:"));
+	my $resnamea = $atomTypes->{$a}->[5];
+	my $resnameb = $atomTypes->{$b}->[5];
+	my $resnamec = $atomTypes->{$c}->[5];
+	my $resnamed = $atomTypes->{$d}->[5];
     	my $resTypea = $atomTypes->{$a}->[1];
 	my $resTypeb = $atomTypes->{$b}->[1];
 	my $resTypec = $atomTypes->{$c}->[1];
@@ -184,7 +188,10 @@ sub adjustFactorsHelper1
 		}
 
 		$count*=$relativeRatio;
-        	${$sum}+=$count;
+		unless($residues{$resnamea}->{'atomCount'}==0 || $residues{$resnameb}->{'atomCount'}==0
+			|| $residues{$resnamec}->{'atomCount'}==0 || $residues{$resnamed}->{'atomCount'}==0){
+        		${$sum}+=$count;
+		}
         	set($diheArr,5,$i,$count);
 
 		push(@tempArr,pdl($count,1));
