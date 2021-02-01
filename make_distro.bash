8456d3f989510e0b36269438dab61bc5c436f024
Noel <jnoel@daumke59.localdomain>
2021-02-01 18:48:27 +0100

    make_distro.bash will make a new MANIFEST file with parameter 'manifest'. If no parameter is given then it acts as before.

--- make_distro.bash
+++ make_distro.bash
@@ -1,17 +1,71 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]
+then
+
 if [ -d distro ]
 then
 	echo directory distro exists. quitting
 else
-
 	mkdir distro
 	for name in `cat MANIFEST`
 	do
-		cp --parent $name distro
-		sed "s/optim//g" configure.smog2 > distro/configure.smog2
+		if command -v gcp &> /dev/null;
+		then
+			gcp --parent $name distro
+		else
+			cp --parent $name distro
+		fi
 	done
+	sed "s/optim//g" configure.smog2 > distro/configure.smog2
 	echo done making distro
 
 	echo DONT FORGET TO UPDATE VERSION NUMBER IN smogv2
+fi
+
+else
+
+if [ $1 = build_manifest ]
+then
+
+# Steps:
+# 1. makes a don't keep file with names of files
+#     to remove
+# 2. build grep remove line
+# 3. add all files and remove the files that we don't want 
+# 4. go back and check MANIFEST yourself and make sure 
+#     nothing unwanted made it in
+
+if [ -f MANIFEST.bak ]
+then
+echo "ERROR: Backup MANIFEST already exists"
+echo "Quitting so that I don't overwrite it"
+exit
+fi
+
+cp MANIFEST MANIFEST.bak
+#full paths!!
+cat > dontkeep << EOF
+./SBM_AA
+./SBM_AA+gaussian
+./SBM_calpha
+./SBM_calpha+gaussian
+./bin
+.git
+./dontkeep
+./MANIFEST.bak
+EOF
+
+remove="dummyXYZ"
+for token in `cat dontkeep`
+do
+remove=$remove"\\|$token"
+done
+rm dontkeep
+
+find . -type f | grep -v "$remove" | sed "s/\.\///g" > MANIFEST
+
+else
+echo Error: parameter $1 not recognized
+fi
 fi
