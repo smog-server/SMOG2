74c57812fce550c5f67ffc44345ce8ca20ad900c
Paul <whitford@laptop>
2021-07-09 23:47:54 -0400

    common: removed perl4smog check, since this is now in the wrapper

--- src/smog_common.pm
+++ src/smog_common.pm
@@ -90,19 +90,16 @@ sub warnsummary
 
 sub checkForModules {
 	my $checkPackage; my $sum=0;
-	$checkPackage=`echo \$perl4smog`;
-	if(length($checkPackage) < 2) { print "\nFailed to launch.\n\nEnvironment variable perl4smog not set, maybe you need to edit the configure.smog2 script and run it with \"source configure.smog2\"\n"; $sum++;}else{
-		$checkPackage=`\$perl4smog -e "use XML::Simple" 2>&1 `;
-		if(length($checkPackage) > 0) { print "Perl module XML::Simple not installed!\n"; $sum++;}
-		$checkPackage=`\$perl4smog -e "use XML::LibXML" 2>&1 `;
-		if(length($checkPackage) > 0) { smog_quit("Perl module XML::LibXML not installed. If you are using openSMOG, then SMOG2 will exit without completing.",1)}
-		$checkPackage=`\$perl4smog -e "use XML::Validator::Schema" 2>&1 `;
-		if(length($checkPackage) > 0) { print "Perl module XML::Validator::Schema not installed!\n"; $sum++;}
-		$checkPackage=`\$perl4smog -e "use Exporter" 2>&1 `;
-		if(length($checkPackage) > 0) { print "Perl module Exporter not installed!\n"; $sum++;}
-		$checkPackage=`\$perl4smog -e "use PDL" 2>&1 `;
-		if(length($checkPackage) > 0) { print "Perl Data Language not installed!\n"; $sum++;}
-	}
+	$checkPackage=`\$perl4smog -e "use XML::Simple1" 2>&1 `;
+	if(length($checkPackage) > 0) { print "Perl module XML::Simple not installed!\n"; $sum++;}
+	$checkPackage=`\$perl4smog -e "use XML::LibXML" 2>&1 `;
+	if(length($checkPackage) > 0) { smog_quit("Perl module XML::LibXML not installed. If you are using openSMOG, then SMOG2 will exit without completing.",1)}
+	$checkPackage=`\$perl4smog -e "use XML::Validator::Schema" 2>&1 `;
+	if(length($checkPackage) > 0) { print "Perl module XML::Validator::Schema not installed!\n"; $sum++;}
+	$checkPackage=`\$perl4smog -e "use Exporter" 2>&1 `;
+	if(length($checkPackage) > 0) { print "Perl module Exporter not installed!\n"; $sum++;}
+	$checkPackage=`\$perl4smog -e "use PDL" 2>&1 `;
+	if(length($checkPackage) > 0) { print "Perl Data Language not installed!\n"; $sum++;}
 	$checkPackage=`which java `;
 	if(length($checkPackage) < 1) { print "Java might not be installed. This package assumes Java 1.7 or greater is in the path as 'java'.\n"; $sum++;}
 	if($sum > 0) { smog_quit("Need above packages before smog2, smog-check and smog tools can run. Some hints may be in the SMOG 2 manual."); }
