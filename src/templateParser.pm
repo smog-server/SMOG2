0caad96df6e16cce494cdd7172cff1a3acd70892
Paul Whitford <p.whitford@neu.edu>
2021-08-19 22:19:05 -0400

    v2.4.1

--- src/templateParser.pm
+++ src/templateParser.pm
@@ -53,7 +53,7 @@ our $OpenSMOGpothash;
 
 my %SMOGversions;
 my $smi=0;
-foreach my $ver("2.0", "2.0.1", "2.0.2", "2.0.3", "2.1", "2.2", "2.3", "2.4"){
+foreach my $ver("2.0", "2.0.1", "2.0.2", "2.0.3", "2.1", "2.2", "2.3", "2.4", "2.4.1"){
  $SMOGversions{$ver}=$smi;
  $smi++; 
 }
