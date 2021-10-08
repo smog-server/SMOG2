4c7eba2bff5c9011e3bab31fdc57b42cea169cf5
Paul <whitford@laptop>
2021-10-08 00:15:27 -0400

    v2.5beta restored

--- src/templateParser.pm
+++ src/templateParser.pm
@@ -53,7 +53,7 @@ our $OpenSMOGpothash;
 
 my %SMOGversions;
 my $smi=0;
-foreach my $ver("2.0", "2.0.1", "2.0.2", "2.0.3", "2.1", "2.2", "2.3", "2.4", "2.4.1", "2.4.2"){
+foreach my $ver("2.0", "2.0.1", "2.0.2", "2.0.3", "2.1", "2.2", "2.3", "2.4", "2.4.1", "2.4.2","2.5beta"){
  $SMOGversions{$ver}=$smi;
  $smi++; 
 }
