6884b30125466012ff0ba314da8027f026be402d
Paul <whitford@laptop>
2022-09-04 15:01:21 -0400

    message update

--- docker.create.bash
+++ docker.create.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # this script will build a new docker container from the current smog2 repo
+containername=smog2:gitversion
 if command -v gcp &> /dev/null;
 then
         CP=gcp
@@ -24,5 +25,21 @@ then
 	exit 
 fi
 
-docker build --no-cache -t smog2:gitversion .
+docker build --no-cache -t $containername .
 rm -r docker.tmp
+
+echo "
+Done building container $containername
+
+It is strongly recommended that you run smog-check
+to ensure that the container is properly configured/
+
+To do this, run the container. Once inside, issue the commands:
+cd /opt/smog2/SMOG-CHECK
+./smog-check
+./smog-tool-check
+
+Since containers are static, you will only need to run
+smog-check the first time.
+
+"
