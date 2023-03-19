83dbee7324528a3b0bf1f58b00d03d1f0351b8f8
Paul <p.whitford@neu.edu>
2023-03-19 14:15:19 -0400

    docker ed smog-opensmog update

--- docker/docker.create.educational.bash
+++ docker/docker.create.educational.bash
@@ -8,13 +8,13 @@ containername=$docker_username/smogopensmog:$tag
 if [ "$option" == "cross" ]
 then
 	# this build a cross-platform container. It also pushes the container to your DockerHub repo. For a simpler build, see the commented command, below
-	echo Will attempt to build and push a cross-platform version of the SMOG 2 container for DockerHub account $docker_username and SMOG version $tag
+	echo Will attempt to build and push a cross-platform version of the SMOG 2 and OpenSMOG container for DockerHub account $docker_username and SMOG version $tag
 	docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --no-cache -t $containername -f Dockerfile.SMOG-OpenSMOG.$tag .
 	# optionally add more tags
 	#docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --no-cache -t $containername -t smogserver/smog2:latest -t smogserver/smog2:stable -f Dockerfile.SMOG-OpenSMOG.$tag .
 elif [ "$option" == "" ]
 then
-	echo Will attempt to build a local native-platform version of the SMOG 2 container: container called $containername
+	echo Will attempt to build a local native-platform version of the SMOG 2 and OpenSMOG container: container called $containername
 	docker build --no-cache -t $containername -f Dockerfile.SMOG-OpenSMOG.$tag .
 else
 
